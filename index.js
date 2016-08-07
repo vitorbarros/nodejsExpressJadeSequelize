501e29f9c9b2e905c1f10c88435608bd63f0880c
Vitor Barros <vitormonteirodebarros@gmail.com>
2016-08-07 18:16:54 -0300

    inciando o projeto

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.set('Views', '/views');
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res){
+  res.render('index', {
+    message: 'Teste'
+  });
+});
+
+app.listen(3000, '127.0.0.1', function(){
+  console.log('started');
+});
