1abb254dd271f60fd9fbda4d5d4b6ba4e02f4eaa
Vitor Barros <vitormonteirodebarros@gmail.com>
2016-08-10 20:22:09 -0300

    finalizando crud basico

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var Sequelize = require('sequelize');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 var app = express();
 
+//defining template engine
 app.set('Views', '/views');
 app.set('view engine', 'pug');
-//11.32
+//data parse to json
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+//method-override
+app.use(methodOverride(function(req, res){
+  if(req.body && typeof req.body == 'object' && '_method' in req.body){
+    var method = req.body._method;
+    delete req.body._method;
+    return method;
+  }
+}));
 
 //database connection
 var sequelize = new Sequelize('mysql://root:root@localhost:3306/expressjs');
@@ -54,7 +64,57 @@ app.get('/courses/create', function(req, res){
     });
 });
 app.post('/courses/create', function(req, res){
-    console.log(req.body);
+    Courses
+      .create(req.body)
+      .then(function(){
+        res.render('new_course', {
+          message:'The course has been created'
+        });
+      })
+      .catch(function(err){
+        console.log(err);
+      });
+});
+app.delete('/courses/:id', function(req, res){
+  Courses
+    .destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(function(){
+      res.redirect('/courses/');
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+});
+app.get('/courses/edit/:id', function(req,res){
+  Courses
+    .findById(req.params.id)
+    .then(function(result){
+      res.render('edit_courses',{
+        message: 'Edit course ' + req.params.id,
+        data: result
+      });
+    })
+    .catch(function(err){
+      console.log(err);
+    });
+});
+app.put('/courses/edit/:id', function(req, res){
+  Courses
+    .update(req.body, {
+      where:{
+        id: req.params.id
+      }
+    })
+    .then(function(){
+        res.redirect('/courses/');
+    })
+    .catch(function(err){
+      console.log(err);
+    });
 });
 app.get('/courses/:id', function(req, res){
 Courses
